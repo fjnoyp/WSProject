98b8de94ae40fd632515864ee538a21e4680581d
kylcheng@gmail.com <16ksc2@tcl216-12.cs.williams.edu>
2016-01-06 11:07:02 -0500

    adding game.js

--- assets/game.js
+++ assets/game.js
@@ -1,1 +1,19 @@
 console.log("hello console");
+window.onload = function() {
+    console.log("starting WSRL - window loaded");
+    // Check if rot.js can work on this browser
+    if (!ROT.isSupported()) {
+        alert("The rot.js library isn't supported by your browser.");
+    } else {
+        // Initialize the game
+        Game.init();
+
+        // Add the containers to our HTML page
+        document.getElementById('wsrl-main-display').appendChild(   Game.getDisplay('main').getContainer());
+    }
+};
+var Game = {
+  init: function () {
+    console.log("WSRL Live Initialization");
+  }
+};
